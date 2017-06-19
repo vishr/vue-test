da7865880ab7e40c6f58bfb51ad52a343eb5a43f
Vishal Rana <vr@labstack.com>
2017-06-18 17:58:55 -0700

    First commit
    Signed-off-by: Vishal Rana <vr@labstack.com>

---
+++ src/App.vue
@@ -0,0 +1,27 @@
+<template>
+  <div id="app">
+    <img src="./assets/logo.png">
+    <router-link to="/hello">Hello</router-link>
+    <keep-alive include="Helloo">
+        <router-view></router-view>
+      </keep-alive>
+    </transition>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'app'
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
